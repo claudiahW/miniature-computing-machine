5cdf0f6fd8d9fd9235ae5ce09918bd08da08276c
claudiahW <murungiclaud@gmail.com>
2024-09-23 16:23:56 +0300

    add necessary icons

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -12,8 +12,29 @@ const Sidebar = () => {
           <img src={assets.plus_icon} alt="" />
           <p>New Chat</p>
         </div>
+
+        <div className="recent">
+          <p className="recent-title">Recent</p>
+          <div className="recent-entry">
+            <img src={assets.message_icon} alt="" />
+            <p>What is react...</p>
+          </div>
+        </div>
       </div>
-      <div className="bottom"></div>
+      <div className="bottom">
+        <div className="bottom-item">
+          <img src={assets.question_icon} alt="" />
+          <p>Help</p>
+        </div>
+        <div className="bottom-item">
+          <img src={assets.history_icon} alt="" />
+          <p>Activity</p>
+        </div>
+        <div className="bottom-item">
+          <img src={assets.setting_icon} alt="" />
+          <p>Settings</p>
+        </div>
+      </div>
     </div>
   );
 };
